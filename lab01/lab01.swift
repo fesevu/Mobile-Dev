275cc3cb3ae2b70a5f0ddc9314ef974de0d9b051
fesevu <fesevu@gmail.com>
2024-04-05 01:20:34 +0300

    переменные и типы данных

--- lab01/lab01.swift
+++ lab01/lab01.swift
@@ -1,3 +1,15 @@
+print("Hello, world!")
+
+var myVariable = 42 //переменная
+myVariable = 50
+let myConstant = 42 //константа
+let explicitDouble: Double //обьвление с указанием типа данных
+explicitDouble = 45
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
 let name = "Многострочные литералы строк"
 
 let quotation = """
